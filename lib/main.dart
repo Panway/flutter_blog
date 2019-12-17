00a70cb4ab781f6c1a5ea7c55951dd13b8da1e44
WeirdPan <wooodypan@foxmail.com>
2019-12-17 15:14:12 +0800

    https://www.apple.com.cn/cn/ demo

--- lib/main.dart
+++ lib/main.dart
@@ -28,15 +28,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -44,68 +35,187 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final _headerFont = const TextStyle(
+        fontSize: 18.0, fontWeight: FontWeight.w400, color: Colors.white70);
+    //ListView要用Scafford包起来，不然文字会出现两个黄色下划线
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        body: ListView(children: [
+      Container(
+        height: 55,
+        padding: const EdgeInsets.only(left: 18),
+        color: Colors.black87,
+        child: Row(
+            // backgroundColor: Colors.white,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text("", style: _headerFont),
+              Center(child: Text("Mac", style: _headerFont)),
+              Center(child: Text("iPad", style: _headerFont)),
+              Center(child: Text("iPhone", style: _headerFont)),
+              Center(child: Text("Watch", style: _headerFont)),
+              Center(child: Text("Music", style: _headerFont)),
+              Center(child: Text("技术支持", style: _headerFont)),
+              Icon(
+                Icons.search,
+                color: Colors.white,
+              ),
+              Icon(
+                Icons.shopping_basket,
+                color: Colors.white,
+              ),
+            ]),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+      Container(
+        color: Colors.white,
+        // height: 200,
+        child: ShoppingListItem(
+          product: new Product(
+              imageURL:
+                  "https://www.apple.com.cn/cn/home/images/heroes/holiday-2019/set-a/endframe__dpqgyay5g30i_large_2x.jpg",
+              title: "来这里，包好礼。",
+              subTitle: "",
+              desc: ""),
+          inCart: true,
+          onCartChanged: null,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      Container(
+        color: Colors.white,
+        // height: 200,
+        child: ShoppingListItem(
+          product: new Product(
+              imageURL:
+                  "https://www.apple.com.cn/v/home/er/images/heroes/iphone-11-pro/hero__dvsxv8smkkgi_medium_2x.jpg",
+              title: "iPhone11 Pro",
+              subTitle: "摄像头、显示屏、性能，样样Pro如其名。",
+              desc: "折抵换购，仅RMB 258/月或RMB 6199起。"),
+          inCart: false,
+          onCartChanged: null,
+        ),
+      ),
+      Container(
+        color: Colors.white,
+        // height: 200,
+        child: ShoppingListItem(
+          product: new Product(
+              imageURL:
+                  "https://www.apple.com.cn/v/home/er/images/heroes/iphone-11/hero__dvsxv8smkkgi_medium_2x.jpg",
+              title: "Phone11",
+              subTitle: "一切都刚刚好。",
+              desc: "折抵换购，仅RMB 154/月或RMB 3699起。"),
+          inCart: true,
+          onCartChanged: null,
+        ),
+      ),
+      // Align(
+      //   widthFactor: 2,
+      //   heightFactor: 2,
+      //   alignment: Alignment.center,
+      //   child: FlutterLogo(
+      //     size: 60,
+      //   ),
+      // ),
+    ]));
+  }
+}
+
+class Product {
+  const Product({this.imageURL, this.title, this.subTitle, this.desc});
+  final String imageURL;
+  final String title;
+  final String subTitle;
+  final String desc;
+}
+
+typedef void CartChangedCallback(Product product, bool inCart);
+
+class ShoppingListItem extends StatelessWidget {
+  ShoppingListItem({Product product, this.inCart, this.onCartChanged})
+      : product = product,
+        super(key: ObjectKey(product));
+
+  final Product product;
+  final bool inCart;
+  final CartChangedCallback onCartChanged;
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      // backgroundColor: Colors.green,
+      alignment: AlignmentDirectional.topCenter, //const Alignment(0.6, 0.6),
+      // fit: StackFit.loose,
+      children: [
+        Container(
+            constraints: BoxConstraints(
+                minWidth: double.infinity, //宽度尽可能大
+                minHeight: 450.0 //最小高度为50像素
+                ),
+            // width: MediaQuery.of(context).size.width,
+            // height: MediaQuery.of(context).size.width / 16.0 * 9.0,
+            // child: AspectRatio(
+            //     aspectRatio: 16.0 / 9.0, // 设置宽高比为16:9
+            child: Image.network(
+              product.imageURL,
+              fit: BoxFit.cover,//BoxFit.fill,
+            )
+            // )
+            ),
+        Column(children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 38.0),
+            child: Text(
+              product.title,
+              style: TextStyle(
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
+                color: _getColor(context),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 5.0),
+            child: Text(
+              product.subTitle,
+              style: TextStyle(
+                fontSize: 15.0,
+                // fontWeight: FontWeight.bold,
+                color: _getColor(context),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 8.0),
+            child: Text(
+              product.desc,
+              style: TextStyle(
+                fontSize: 12.0,
+                // fontWeight: FontWeight.bold,
+                color: Colors.grey,
+              ),
+            ),
+          )
+        ]),
+      ],
+    );
+  }
+
+  Color _getColor(BuildContext context) {
+    // The theme depends on the BuildContext because different parts of the tree
+    // can have different themes.  The BuildContext indicates where the build is
+    // taking place and therefore which theme to use.
+
+    return inCart
+        ? Colors.black
+        : Colors.white; //Theme.of(context).primaryColor;
+  }
+
+  TextStyle _getTextStyle(BuildContext context) {
+    if (!inCart) return null;
+
+    return TextStyle(
+      color: Colors.black54,
+      decoration: TextDecoration.lineThrough,
     );
   }
 }
